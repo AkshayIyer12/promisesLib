191942e17d32cd662b907974930516979bd16c8f
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-29 07:40:01 +0530

    Add resolve function

--- myPromise.js
+++ myPromise.js
@@ -1,48 +1,93 @@
-// class MyPromise {
-//   constructor (executor) {
-//     this.func = executor
-//   }
-//   resolve (a) {
-//     return a
-//   }
-//   reject (err) {
-//     throw Error(err)
-//   }
-//   then (b) {
-//     if (this.func(this.resolve)) {
-//       return b(20)
-//     }
-//     return b(100)
-//   }
-// }
+let pending = 0, fulfilled = 1, rejected = 2
 
-let MyPromise = function (executor) {
-  this.__func = executor
-}
-MyPromise.of = function (x) {
-  return new MyPromise(x)
-}
-MyPromise.prototype.resolve = function (a) {
-  return a
-}
-MyPromise.prototype.reject = function (err) {
-  return Error(err)
-}
-MyPromise.prototype.then = function (vfunc) {
-  return this.__func(this.__proto__.resolve(vfunc(20)))
-}
-MyPromise.prototype.catch = function (vfunc) {
-  return this.__func(this.__proto__.reject(vfunc('Error')))
-}
-console.log(MyPromise.of((resolve, reject) => {
-  let timeString = Date.now()
-  setTimeout(() => {
-    console.log(resolve, reject)
-    return resolve(timeString)
-  }, 2000)
-}).then(display))
-//.catch(display))
+function MyPromise (fn) {
+  if (typeof this !== 'object') {
+    throw new TypeError('Promises must be constructed via new')
+  }
+  if (typeof fn !== 'function') {
+    throw new TypeError('fn must be a function')
+  }
+  let state = pending
+  let value = null
+  let handlers = []
 
-function display (value) {
-  return value
+  function fulfill (result) {
+    state = fulfilled
+    value = result
+    handlers.map(handle)
+    handlers = null
+  }
+
+  function reject (error) {
+    state = rejected
+    value = error
+    handlers.map(handle)
+    handlers = null
+  }
+
+  function resolve (result) {
+    try {
+      let then = getThen(result)
+      if (then) {
+        doResolve(then.bind(result), resolve, reject)
+        return
+      }
+      fulfill(result)
+    } catch (e) {
+      reject(e)
+    }
+  }
+
+  function handle (handler) {
+    if (state === pending) {
+      handlers.push(handler)
+    } else {
+      if (state === fulfilled && 
+      typeof handler === 'function') {
+        handler.onFulfilled(value)
+      }
+      if (state === rejected && 
+      typeof handler === 'function') {
+        handler.onRejected(value)
+      }
+    }
+  }
+  this.done = function (onFulfilled, onRejected) {
+    process.nextTick(function () {
+      handle({
+        onFulfilled,
+        onRejected
+      })
+    })
+  }
+  doResolve(fn, resolve, reject)
+}
+
+function getThen (result) {
+  if (result && (typeof result === 'object' || typeof result === 'function')) {
+    let then = result.then
+    if (typeof then === 'function') {
+      return then
+    }
+  }
+  return null
+}
+
+function doResolve (fn, onFulfilled, onRejected) {
+  let done = false
+  try {
+    fn(function (value) {
+      if (done) return
+      done = true
+      onFulfilled(value)
+    }, function (reason) {
+      if (done) return
+      done = true
+      onRejected(reason)
+    })
+  } catch (ex) {
+    if (done) return
+    done = true
+    onRejected(ex)
+  }
 }
