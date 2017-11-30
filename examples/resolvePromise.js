1f7e6a37ac097e4af946567ead14a21950c904bd
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-30 23:55:40 +0530

    Update example

--- examples/resolvePromise.js
+++ examples/resolvePromise.js
@@ -3,16 +3,23 @@ let MyPromise = require('../lib/myPromiseMethods')
 let p1 = MyPromise.resolve(3)
 let p2 = MyPromise.resolve({
   then: function (onFulfill, onReject) {
-    onFulfill('fulfilled!')
+    onFulfill('Hey! I am fulfilled now')
   }
 })
 let p3 = MyPromise.resolve(new MyPromise(function (resolve, reject) {
-  resolve(10)
+  reject('Show me some Error')
 }))
-
+let p4 = MyPromise.resolve(new MyPromise(function (resolve, reject) {
+  resolve('Ahoy! Im here')
+}))
 p1.then(display)
 p2.then(display)
-p3.then(display)
+p3.then(display).catch(function (err) {
+  console.log(err)
+})
+p4.then(display).catch(function (err) {
+  console.log(err)
+})
 
 function display (value) {
   console.log(value)
