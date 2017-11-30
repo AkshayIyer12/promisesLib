acdca18656fafbe50ae7a9ffaeaf00530e3d02f8
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-30 23:55:25 +0530

    Update example

--- examples/rejectPromise.js
+++ examples/rejectPromise.js
@@ -1,7 +1,8 @@
 let MyPromise = require('../lib/myPromiseMethods')
 
-MyPromise.reject('fail').then(function (error) {
+MyPromise.reject('fail').then(function (result) {
   // not called
+  console.log('Im should not be here')
 }, function (error) {
   console.log(error) // Stacktrace
 })
